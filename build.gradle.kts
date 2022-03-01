b9c1c4127f3542fa7b1f3de74ac71b70c478e282
Lucho <luchi_m.022@hotmail.com>
2022-02-28 23:03:51 -0300

    new yml file for CI/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,7 +106,8 @@ fun BaseExtension.applyBaseCommons() = apply {
 
     signingConfigs {
         create("release") {
-            storeFile = file("secret/console-app-keystore.jks")
+            print(project.property("KEYSTORE_FILE").toString())
+            storeFile = file(project.property("KEYSTORE_FILE").toString())
             keyAlias =  project.property("KEY_ALIAS").toString()
             keyPassword = project.property("KEY_PASSWORD").toString()
             storePassword = project.property("KEYSTORE_PASSWORD").toString()
